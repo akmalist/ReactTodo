827a861bef3d7080f21e7ff5f6446a87d4b640b8
Aki <akmalist@gmail.com>
2019-12-23 10:45:47 -0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,41 @@
 import React, { useState } from "react";
 import TodoItem from "./TodoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  //1. When new text is written into the input, its state should be saved.
-  const [item, setItem] = useState("");
-  const [itemSubmit, setItemSumpit] = useState([]);
+  const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const { value } = event.target;
-    setItem(value);
-  }
-
-  //2. When the add button is pressed, the current data in the input should be
-  //added to an array.
-  function addItem() {
-    setItemSumpit(prevItems => {
-      return [...prevItems, item];
+  function addTodo(todo) {
+    setItems(prevItems => {
+      return [...prevItems, todo];
     });
-    setItem("");
   }
 
-  function handleClickCross(id) {
-    setItemSumpit(prevItems => {
-      return prevItems.filter((prevItem, index) => {
+  function removeItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((value, index) => {
         return index !== id;
       });
     });
   }
-
-  //3. The <ul> should display all the array items as <li>s
   return (
     <div className="container">
       <div className="heading">
-        <h1> To-Do List</h1>
+        <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input
-          onChange={handleChange}
-          value={item}
-          name={item}
-          type="text"
-          placeholder="Add item "
-        />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
+        <InputArea addItem={addTodo} />
       </div>
       <div>
         <ul>
-          {itemSubmit.map((todoItem, index) => {
-            return (
-              <TodoItem
-                key={index}
-                id={index}
-                text={todoItem}
-                onChecked={handleClickCross}
-              />
-            );
-          })}
+          {items.map((todoItem, index) => (
+            <TodoItem
+              key={index}
+              id={index}
+              deleteItem={removeItem}
+              Todo={todoItem}
+            />
+          ))}
         </ul>
       </div>
     </div>
